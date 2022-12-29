5124b82705dfc2229c0879c9d0b4984763c29b06
joona95 <joona95@naver.com>
2022-12-29 21:46:58 +0900

    인프런 자바 알고리즘 문제풀이 입문 - 문제1 문자 찾기 문제풀이 코드 추가

---
+++ src/Problem_1_문자_찾기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Problem_1_문자_찾기 {
+
+    static int solution(String str, char c) {
+        int answer = 0;
+        str = str.toUpperCase();
+        c = Character.toUpperCase(c);
+        /*
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == c)
+                answer++;
+        }
+         */
+        for (char x : str.toCharArray()) {
+            if (x == c)
+                answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        char c = in.next().charAt(0);
+        int result = solution(str, c);
+        System.out.println(result);
+    }
+}
