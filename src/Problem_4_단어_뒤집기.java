a4811e3aea94615f42fd8669f56286008e757138
joona95 <joona95@naver.com>
2022-12-30 20:40:44 +0900

    인프런 자바 알고리즘 문제풀이 입문 - 문제4 단어 뒤집기 문제풀이 코드 추가

---
+++ src/Problem_4_단어_뒤집기.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Problem_4_단어_뒤집기 {
+
+    /*
+    static String solution(String word) {
+        String answer = "";
+        int len = word.length() - 1;
+        for (int i = len; i >= 0; i--) {
+            answer += word.charAt(i);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        for (int i = 0; i < num; i++) {
+            String word = in.next();
+            String result = solution(word);
+            System.out.println(result);
+        }
+    }
+     */
+
+    static ArrayList<String> solution(int num, String[] words) {
+        ArrayList<String> answer = new ArrayList<>();
+        /*
+        for (String word : words) {
+            String tmp = new StringBuilder(word).reverse().toString();
+            answer.add(tmp);
+        }
+        */
+        for (String word : words) {
+            char[] s = word.toCharArray();
+            int lt = 0, rt = word.length() - 1;
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        String[] words = new String[num];
+        for (int i = 0; i < num; i++) {
+            words[i] = in.next();
+        }
+        for (String word : solution(num, words)) {
+            System.out.println(word);
+        }
+    }
+}
