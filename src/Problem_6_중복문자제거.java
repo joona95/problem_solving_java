64c013340c584124623ae7f6c04f30b1c59af234
joona95 <joona95@naver.com>
2022-12-31 09:30:08 +0900

    인프런 자바 알고리즘 문제풀이 입문 - 문제6 중복문자제거 문제풀이 코드 추가

---
+++ src/Problem_6_중복문자제거.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Problem_6_중복문자제거 {
+    static String solution(String str) {
+        String answer = "";
+        /*
+        boolean[] isAlphabetExist = new boolean[30];
+        for(char c : str.toCharArray()) {
+            if(!isAlphabetExist[c - 'a']) {
+                answer += c;
+                isAlphabetExist[c - 'a'] = true;
+            }
+        }
+         */
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i)) == i)
+                answer += str.charAt(i);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        String result = solution(str);
+        System.out.println(result);
+    }
+}
