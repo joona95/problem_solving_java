b437a620976079855e5b28b84080ed231260d9cd
joona95 <joona95@naver.com>
2022-12-31 09:17:24 +0900

    인프런 자바 알고리즘 문제풀이 입문 - 문제5 특정 문자 뒤집기 문제풀이 코드 추가

---
+++ src/Problem_5_특정_문자_뒤집기.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Problem_5_특정_문자_뒤집기 {
+    static String solution(String str) {
+        String answer = "";
+        char[] s = str.toCharArray();
+        int lt = 0, rt = str.length() - 1;
+        while (lt < rt) {
+            if (!Character.isAlphabetic(s[lt])) {
+                lt++;
+                continue;
+            }
+            if (!Character.isAlphabetic(s[rt])) {
+                rt--;
+                continue;
+            }
+            char tmp = s[lt];
+            s[lt] = s[rt];
+            s[rt] = tmp;
+            lt++;
+            rt--;
+        }
+        answer = String.valueOf(s);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        String result = solution(str);
+        System.out.println(result);
+    }
+}
