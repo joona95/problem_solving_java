ba10babbfd156500bb0eea00cb30d04a76238680
joona95 <joona95@naver.com>
2023-01-06 07:48:10 +0900

    인프런 자바 알고리즘 문제풀이 입문 - 문제5 소수(에라토스테네스 체) 문제풀이 코드 추가

---
+++ src/Array/Problem_5_소수_에라토스테네스_체.java
@@ -0,0 +1,37 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Problem_5_소수_에라토스테네스_체 {
+    static int solution(int n) {
+        int answer = 0;
+        /*
+        for (int i = 2; i < n; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+                answer++;
+        }
+         */
+        int[] arr = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            if (arr[i] == 0) {
+                answer++;
+                for (int j = i; j <= n; j = j + i)
+                    arr[j] = 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(solution(n));
+    }
+}
