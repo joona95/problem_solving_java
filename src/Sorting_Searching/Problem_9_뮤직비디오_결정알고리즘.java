4a7057af580eed474fbbb95d47db25142fc749ad
joona95 <waffle@vanpl.co.kr>
2023-02-08 08:08:53 +0900

    인프런 자바 알고리즘 문제풀이 입문 - 문제9 뮤직비디오(결정알고리즘) 문제풀이 코드 추가

---
+++ src/Sorting_Searching/Problem_9_뮤직비디오_결정알고리즘.java
@@ -0,0 +1,47 @@
+package Sorting_Searching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem_9_뮤직비디오_결정알고리즘 {
+
+    static int count(int[] arr, int capacity) {
+        int cnt = 1, sum = 0;
+        for (int x : arr) {
+            if (sum + x > capacity) {
+                cnt++;
+                sum = x;
+            } else {
+                sum += x;
+            }
+        }
+        return cnt;
+    }
+
+    static int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) <= m) {
+                answer = mid;
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(solution(n, m, arr));
+    }
+}
