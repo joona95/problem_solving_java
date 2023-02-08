920a9e05cb0bdee2d7766272e529c71af33f9508
joona95 <waffle@vanpl.co.kr>
2023-02-09 08:07:35 +0900

    인프런 자바 알고리즘 문제풀이 입문 - 문제10 마구간 정하기(결정알고리즘) 문제풀이 코드 추가

---
+++ src/Sorting_Searching/Problem_10_마구간_정하기_결정알고리즘.java
@@ -0,0 +1,46 @@
+package Sorting_Searching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem_10_마구간_정하기_결정알고리즘 {
+    static int count(int[] arr, int dist) {
+        int cnt = 1;
+        int ep = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - ep >= dist) {
+                cnt++;
+                ep = arr[i];
+            }
+        }
+        return cnt;
+    }
+
+    static int solution(int n, int c, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 1, rt = arr[n - 1];
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) >= c) {
+                answer = mid;
+                lt = mid + 1;
+            } else {
+                rt = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int c = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(solution(n, c, arr));
+    }
+}
