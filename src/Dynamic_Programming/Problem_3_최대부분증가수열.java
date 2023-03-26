ab5e1b9ce12ffbce9185ca8f7bb28909de0fe409
joona95 <waffle@vanpl.co.kr>
2023-03-26 16:20:38 +0900

    인프런 자바 알고리즘 문제풀이 입문 - 문제3 최대부분증가수열(LIS) 문제풀이 코드 추가

---
+++ src/Dynamic_Programming/Problem_3_최대부분증가수열.java
@@ -0,0 +1,34 @@
+package Dynamic_Programming;
+
+import java.util.Scanner;
+
+public class Problem_3_최대부분증가수열 {
+    static int[] dy;
+
+    static int solution(int[] arr) {
+        int answer = 0;
+        dy = new int[arr.length];
+        dy[0] = 1;
+        for (int i = 1; i < arr.length; i++) {
+            int max = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] < arr[i] && dy[j] > max) {
+                    max = dy[j];
+                }
+            }
+            dy[i] = max + 1;
+            answer = Math.max(answer, dy[i]);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+}
