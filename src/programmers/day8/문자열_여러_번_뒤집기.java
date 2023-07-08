f800be27f75bf88b2ad46c30f60e580cdbecc2d6
joona95 <waffle@vanpl.co.kr>
2023-07-09 08:52:00 +0900

    프로그래머스 기초 트레이닝 Day8 - 조건문, 문자열 문제 풀이

---
+++ src/programmers/day8/문자열_여러_번_뒤집기.java
@@ -0,0 +1,22 @@
+package programmers.day8;
+
+public class 문자열_여러_번_뒤집기 {
+    static public String solution(String my_string, int[][] queries) {
+        char[] answer = my_string.toCharArray();
+        for (int[] query : queries) {
+            for (int i = query[0]; i <= (query[0] + query[1]) / 2; i++) {
+                char tmp = answer[i];
+                answer[i] = answer[query[0] + query[1] - i];
+                answer[query[0] + query[1] - i] = tmp;
+            }
+        }
+        return String.valueOf(answer);
+    }
+
+    //substring하여 stringbuilder에서 reverse하는 방식
+
+    public static void main(String[] args) {
+        System.out.println(solution("rermgorpsam",
+                new int[][]{{2, 3}, {0, 7}, {5, 9}, {6, 10}}));
+    }
+}
