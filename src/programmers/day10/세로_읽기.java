5d45e8fb3820d0219dfe4a9057f5304f9ae40d51
joona95 <waffle@vanpl.co.kr>
2023-07-11 08:36:35 +0900

    프로그래머스 기초 트레이닝 Day10 - 문자열 문제 풀이

---
+++ src/programmers/day10/세로_읽기.java
@@ -0,0 +1,23 @@
+package programmers.day10;
+
+import java.util.Scanner;
+
+public class 세로_읽기 {
+    public static String solution(String my_string, int m, int c) {
+        StringBuilder sb = new StringBuilder();
+        int idx = c - 1;
+        while (idx < my_string.length()) {
+            sb.append(my_string.charAt(idx));
+            idx += m;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String my_string = sc.next();
+        int m = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(solution(my_string, m, c));
+    }
+}
---
+++ src/programmers/day10/접두사인지_확인하기.java
@@ -0,0 +1,16 @@
+package programmers.day10;
+
+import java.util.Scanner;
+
+public class 접두사인지_확인하기 {
+    public static int solution(String my_string, String is_prefix) {
+        return my_string.startsWith(is_prefix) ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String my_string = sc.next();
+        String is_prefix = sc.next();
+        System.out.println(solution(my_string, is_prefix));
+    }
+}
