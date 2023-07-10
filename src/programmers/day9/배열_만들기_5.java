e2fd15f056a02c4f0bfffab5cef73db0cc188353
joona95 <waffle@vanpl.co.kr>
2023-07-10 09:06:38 +0900

    프로그래머스 기초 트레이닝 Day9 - 문자열 문제 풀이

---
+++ src/programmers/day9/배열_만들기_5.java
@@ -0,0 +1,21 @@
+package programmers.day9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 배열_만들기_5 {
+    public static int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> arr = new ArrayList<>();
+        for(String str : intStrs) {
+            int num = Integer.parseInt(str.substring(s, s + l));
+            if (num > k)
+                arr.add(num);
+        }
+        return arr.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static void main(String[] args) {
+        for (int i : solution(new String[] {"0123456789","9876543210","9999999999999"}, 50000, 5, 5))
+            System.out.println(i);
+    }
+}
