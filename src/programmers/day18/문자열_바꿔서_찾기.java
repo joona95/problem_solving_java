ea4b2442079f2517fb54afaa4cd227f2e864fcc6
joona95 <waffle@vanpl.co.kr>
2023-07-19 08:21:57 +0900

    프로그래머스 기초 트레이닝 Day18 - 문자열 문제 풀이

---
+++ src/programmers/day18/문자열_바꿔서_찾기.java
@@ -0,0 +1,20 @@
+package programmers.day18;
+
+public class 문자열_바꿔서_찾기 {
+    public static int solution(String myString, String pat) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i<myString.length();i++) {
+            if (myString.charAt(i) == 'A')
+                sb.append('B');
+            else
+                sb.append('A');
+        }
+        return sb.toString().contains(pat) ? 1 : 0;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(solution("ABBAA", "AABB"));
+        System.out.println(solution("ABAB", "ABAB"));
+    }
+}
