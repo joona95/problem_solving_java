c264635e150cc04715b646e6d77bace98db68d17
joona95 <joona95@naver.com>
2023-07-21 07:22:56 +0900

    프로그래머스 기초 트레이닝 Day20 - 함수(메서드) 문제 풀이

---
+++ src/programmers/day20/배열의_길이를_2의_거듭제곱으로_만들기.java
@@ -0,0 +1,25 @@
+package programmers.day20;
+
+import java.util.Arrays;
+
+public class 배열의_길이를_2의_거듭제곱으로_만들기 {
+    public static int[] solution(int[] arr) {
+        return Arrays.copyOf(arr, getMinPow(arr.length));
+    }
+
+    public static int getMinPow(int num) {
+        int answer = 1;
+        while (num > answer) { //1도 2^0으로 2의 거듭제곱에 속함
+            answer *= 2;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{1, 2, 3, 4, 5, 6})));
+        System.out.println(Arrays.toString(solution(new int[]{58, 172, 746, 89})));
+        System.out.println(Arrays.toString(solution(new int[]{1})));
+        System.out.println(Arrays.toString(solution(new int[]{58, 172, 746, 89, 58, 172, 746})));
+        System.out.println(Arrays.toString(solution(new int[]{58, 172, 746, 89, 58, 172, 746, 58, 172, 746, 89, 58, 172, 746})));
+    }
+}
---
+++ src/programmers/day20/배열의_길이에_따라_다른_연산하기.java
@@ -0,0 +1,21 @@
+package programmers.day20;
+
+import java.util.Arrays;
+
+public class 배열의_길이에_따라_다른_연산하기 {
+    public static int[] solution(int[] arr, int n) {
+        int start = 0;
+        if (arr.length % 2 == 0) {
+            start = 1;
+        }
+        for (int i = start; i < arr.length; i += 2) {
+            arr[i] += n;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{49, 12, 100, 276, 33}, 27)));
+        System.out.println(Arrays.toString(solution(new int[]{444, 555, 666, 777}, 100)));
+    }
+}
