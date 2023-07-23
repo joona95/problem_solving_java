927a654bb2a4fd009ff853f9405424c35f2b8294
joona95 <joona95@naver.com>
2023-07-24 08:58:48 +0900

    프로그래머스 기초 트레이닝 Day23 - 조건문 활용 문제 풀이

---
+++ src/programmers/day23/꼬리_문자열.java
@@ -0,0 +1,15 @@
+package programmers.day23;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class 꼬리_문자열 {
+    public static String solution(String[] str_list, String ex) {
+        return Arrays.stream(str_list).filter(str -> !str.contains(ex)).collect(Collectors.joining());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"abc", "def", "ghi"}, "ef"));
+        System.out.println(solution(new String[]{"abc", "bbc", "cbc"}, "c"));
+    }
+}
---
+++ src/programmers/day23/날짜_비교하기.java
@@ -0,0 +1,27 @@
+package programmers.day23;
+
+import java.util.Arrays;
+
+public class 날짜_비교하기 {
+    public static int solution(int[] date1, int[] date2) {
+        /*
+        if(date1[0] < date2[0])
+            return 1;
+        if(date1[0] == date2[0]) {
+            if (date1[1] < date2[1])
+                return 1;
+            if (date1[1] == date2[1]) {
+                if (date1[2] < date2[2])
+                    return 1;
+            }
+        }
+        return 0;
+         */
+        return Arrays.compare(date1, date2) < 0 ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{2021, 12, 28}, new int[]{2021, 12, 29}));
+        System.out.println(solution(new int[]{1024, 10, 24}, new int[]{1024, 10, 24}));
+    }
+}
