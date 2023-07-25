bb17b53cfaef66f76141940796a29e843f3693e9
joona95 <joona95@naver.com>
2023-07-26 08:56:31 +0900

    프로그래머스 기초 트레이닝 Day25 - 이차원 리스트(배열) 문제 풀이

---
+++ src/programmers/day25/정사각형으로_만들기.java
@@ -0,0 +1,23 @@
+package programmers.day25;
+
+import java.util.Arrays;
+
+public class 정사각형으로_만들기 {
+    public static int[][] solution(int[][] arr) {
+        int n = Math.max(arr.length, arr[0].length);
+        int[][] answer = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i < arr.length && j < arr[i].length) {
+                    answer[i][j] = arr[i][j];
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(solution(new int[][]{{572, 22, 37}, {287, 726, 384}, {85, 137, 292}, {487, 13, 876}})));
+        System.out.println(Arrays.deepToString(solution(new int[][]{{57, 192, 534, 2}, {9, 345, 192, 999}})));
+    }
+}
